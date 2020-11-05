d0badb764e93ea60d137de26ef5f443471a077bd
Suman Das <sumansd166@gmail.com>
2020-11-06 00:54:59 +0530

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import HomepageImage from './components/HomepageImage'
+
+function App() {
+  return (
+    <div className="App">
+    <header className="App-header">
+    <HomepageImage />
+    <p>
+      My first React webpage!
+    </p>
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn React
+    </a>
+  </header>
+    </div>
+  );
+}
+
+export default App;
