2ec2577fdd003cf4a75a5d9fdefba7d96263e009
Suman Das <sumansd166@gmail.com>
2020-11-06 00:57:20 +0530

    Create HomepageImage.js

---
+++ src/components/HomepageImage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function HomepageImage(){
+	
+	const url = 'https://cdn.filestackcontent.com/XYrHCaFGRSaq0EPKY1S6';
+	  return (
+	    <img src="cognizant.png" style={{width: 800}} alt='Image of Cognizant BLR Office' />
+	  );
+	
+}
+
+export default HomepageImage;
